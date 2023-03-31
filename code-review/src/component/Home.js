436e9266ed1e5c07ebc1763fdf5c7a055623bdc1
harish08102004 <harish.boopathi@kalvium.community>
2023-03-31 18:34:17 +0530

    updated

--- code-review/src/component/Home.js
+++ code-review/src/component/Home.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';gi 
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Editor } from '@tinymce/tinymce-react';
 
 function ReportGenerator() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data) => {
-    // Perform analysis on sentence here...
-    console.log(data.sentence);
+  const [tone, setTone] = useState("");
+  const [quality, setQuality] = useState("");
+  const [showOutput, setShowOutput] = useState(false);
+
+  const analyze = async () => {
+    const inputText = document.getElementById('input-text').value;
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci/tone', {
+        prompt: 'Please analyse the following paragraph for its quality attributes and give a score or 0-5 for each quality attribute : "{inputText}". Return the results in this format: <Grammar>#<Punctuation>#<Spelling>#<Clarity>#<Conciseness>#<Coherence>#<Vocabulary>#<Tone>#<Readability>#<Style>',
+        max_tokens: 1,
+        temperature: 0,
+        n: 1,
+        stop: ['\n']
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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'
+        }
+      });
+
+      const data = response.data;
+      const tone = data.choices[0].text.trim();
+      const quality = data.choices[0].attributes.quality.trim();
+
+      setTone(tone);
+      setQuality(quality);
+      setShowOutput(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        Sentence:
-        {errors.sentence && <span>This field is required.</span>}
-      </label>
-      <button type="submit">Generate Report</button>
-    </form>
+    <div>
+      <Editor id="input-text" apiKey="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" />
+      <button onClick={analyze}>Generate Report</button>
+      {showOutput &&
+        <div>
+          <h3>Tone: {tone}</h3>
+          <h3>Quality: {quality}</h3>
+        </div>
+      }
+    </div>
   );
 }
 
---
+++ code-review/src/component/quality.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Quality() {
+  return (
+    <div>
+        <input type='text'></input>
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Quality
